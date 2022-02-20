db1cb4395d246ad8a0a86ae0793339bb2f09c338
nastia0909 <95539304+nastia0909@users.noreply.github.com>
2022-02-20 11:25:48 +0200

    Delete hm4.py

--- hm4.py
+++
@@ -1,6 +0,0 @@
-ticket_number = input()
-checking = [int(ticket_number) for ticket_number in range(0, 1000000)]
-if sum(checking[3:]) == sum(checking[:3]):
-    print('lucky ')
-else:
-    print('unlucky')
